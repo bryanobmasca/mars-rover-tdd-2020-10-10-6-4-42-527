631658de09fdc38cece079fc9e55feeda16a6f1e
bryanobmasca <bryanobmasca@gmail.com>
2020-10-15 17:22:15 +0800

    [MarsRover][Bryan] test: Execute command M with given location(x=0, y=0) and heading("S")

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -40,4 +40,16 @@ class MarsRoverTest {
         assertEquals(0,marsRover.getLocationY());
         assertEquals("E",marsRover.getHeading());
     }
+
+    @Test
+    void should_return_x_0_y_minus1_heading_S_when_execute_given_x_0_y_0_heading_S_and_command_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"S");
+        //when
+        marsRover.executeCommands("M");
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(-1,marsRover.getLocationY());
+        assertEquals("S",marsRover.getHeading());
+    }
 }
