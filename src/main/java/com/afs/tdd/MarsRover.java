76fd29c203a0213158ce2708f862d8ec71b3c679
bryanobmasca <bryanobmasca@gmail.com>
2020-10-15 17:04:19 +0800

    [MarsRover][Bryan] test: Execute command M with given location(x=0, y=0) and heading("N")

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,29 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private final int locationX;
+    private final int locationY;
+    private final String heading;
+
+    public MarsRover(int locationX, int locationY, String heading) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.heading = heading;
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+
+    public String getHeading() {
+        return null;
+    }
+
+    public void executeCommands(String command) {
+
+    }
+}
